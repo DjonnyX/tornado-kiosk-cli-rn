69e4ec405ad605b62d787a7df7ae924e508ea7d0
djonnyx <djonnyx@gmail.com>
2020-10-09 12:49:23 +0300

    feat: inserted ModalSolid component

--- src/components/simple/LanguagePicker.tsx
+++ src/components/simple/LanguagePicker.tsx
@@ -5,6 +5,7 @@ import FastImage from "react-native-fast-image";
 import { ICompiledLanguage } from "@djonnyx/tornado-types";
 import Color from "color";
 import { theme } from "../../theme";
+import { ModalSolid } from "./ModalSolid";
 
 interface ILanguagePickerProps {
     languages: Array<ICompiledLanguage>;
@@ -38,58 +39,25 @@ export const LanguagePicker = React.memo(({ language, languages, onSelect }: ILa
 
     return (
         <View style={{ justifyContent: "center", alignItems: "center", height: 48 }}>
-            <Modal
-                animationType="slide"
-                visible={modalVisible}
-                presentationStyle="fullScreen"
-                statusBarTranslucent={true}
-            >
-                <View style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    position: "absolute",
-                    top: 0,
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    backgroundColor: theme.themes[theme.name].common.modal.background,
-                }}>
-                    <View style={{
-                        margin: 20,
-                        backgroundColor: theme.themes[theme.name].common.modal.window.background,
-                        borderRadius: 8,
-                        padding: 35,
-                        alignItems: "center",
-                        shadowColor: "#000",
-                        shadowOffset: {
-                            width: 0,
-                            height: 2,
-                        },
-                        shadowOpacity: 0.25,
-                        shadowRadius: 3.84,
-                        elevation: 8,
+            <ModalSolid visible={modalVisible}>
+                <FlatList style={{ flexGrow: 0, padding: 12 }} data={languages} renderItem={({ item }) => {
+                    return <TouchableOpacity onPress={() => {
+                        selectHandler(item);
                     }}>
-                        <FlatList style={{ flexGrow: 0, padding: 12 }} data={languages} renderItem={({ item }) => {
-                            return <TouchableOpacity onPress={() => {
-                                selectHandler(item);
-                            }}>
-                                <View style={{ flexDirection: "column", alignItems: "center", justifyContent: "center", marginBottom: 32 }}>
-                                    <FastImage style={{ width: 128, height: 128, borderWidth: 1, borderColor: theme.themes[theme.name].languageModal.item.borderColor, marginBottom: 8 }} source={{
-                                        uri: `file://${item?.resources?.main?.mipmap.x128}`,
-                                    }} resizeMode={FastImage.resizeMode.contain}></FastImage>
-                                    <Text style={{ fontSize: 16, color: theme.themes[theme.name].languageModal.item.textColor }}>
-                                        {
-                                            item?.name
-                                        }
-                                    </Text>
-                                </View>
-                            </TouchableOpacity>
-                        }}>
-                        </FlatList>
-                    </View>
-                </View>
-            </Modal>
+                        <View style={{ flexDirection: "column", alignItems: "center", justifyContent: "center", marginBottom: 32 }}>
+                            <FastImage style={{ width: 128, height: 128, borderWidth: 1, borderColor: theme.themes[theme.name].languageModal.item.borderColor, marginBottom: 8 }} source={{
+                                uri: `file://${item?.resources?.main?.mipmap.x128}`,
+                            }} resizeMode={FastImage.resizeMode.contain}></FastImage>
+                            <Text style={{ fontSize: 16, color: theme.themes[theme.name].languageModal.item.textColor }}>
+                                {
+                                    item?.name
+                                }
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                }}>
+                </FlatList>
+            </ModalSolid>
             <TouchableOpacity style={{ flex: 1, justifyContent: "center", alignItems: "center", width: "100%", height: "100%" }} onPress={pressHandler}>
                 <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "center", borderWidth: 0.5, borderColor: theme.themes[theme.name].languageModal.item.borderColor, padding: 8, overflow: "hidden", width: 44, height: 44, borderRadius: 32 }}>
                     <FastImage style={{ position: "absolute", width: 64, height: 64 }} source={{
--- src/components/simple/index.ts
+++ src/components/simple/index.ts
@@ -3,5 +3,6 @@ export * from "./LanguagePicker";
 export * from "./MyOrderPanel";
 export * from "./OrderTypesPicker";
 export * from "./MenuButton";
+export * from "./ModalSolid";
 export * from "./side-menu";
 export * from "./nav-menu";
