7350b45826490756d1718a629c149e2227603982
djonnyx <djonnyx@gmail.com>
2020-11-18 17:20:32 +0300

    refactor: test apikey changed

--- src/Config.ts
+++ src/Config.ts
@@ -2,12 +2,12 @@ import { IConfig } from "./IConfig";
 
 class Config implements IConfig {
     refServer = {
-        address: "http://192.168.1.86:8080",
+        address: "http://192.168.8.103:8080",
         updateTimeout: 1000,
     };
 
     license = {
-        apiKey: "my-api-key",
+        apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZmFiZGRhMmMwNzcwMjUwNTBlOTlhMmMiLCJlbWFpbCI6ImRqb25ueXhAZ21haWwuY29tIiwiaWF0IjoxNjA1NzA2NDczfQ.ySYmVO50XcgzE99IbyT-mAQRUvbl98JHVlty34FzFWI",
     };
 
     capabilities = {
