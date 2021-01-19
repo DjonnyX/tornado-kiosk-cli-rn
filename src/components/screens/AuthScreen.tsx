51351f5dd8dcd23b88a9bae2821c2e17bf2699a3
djonnyx <djonnyx@gmail.com>
2021-01-20 00:56:07 +0300

    feat: auth screen implemented

--- src/components/screens/AuthScreen.tsx
+++ src/components/screens/AuthScreen.tsx
@@ -1,7 +1,7 @@
 import React, { Dispatch, useCallback, useEffect, useRef, useState } from "react";
 import { StackScreenProps } from "@react-navigation/stack";
 import { ProgressBar } from "@react-native-community/progress-bar-android";
-import { View, Text, Button } from "react-native";
+import { View, TextInput } from "react-native";
 import { MainNavigationScreenTypes } from "../navigation";
 import { IAppState } from "../../store/state";
 import { connect } from "react-redux";
@@ -9,7 +9,6 @@ import { CapabilitiesSelectors, CombinedDataSelectors, SystemSelectors } from ".
 import { CommonActions } from "@react-navigation/native";
 import { theme } from "../../theme";
 import { CapabilitiesActions, NotificationActions } from "../../store/actions";
-import { TextInput } from "react-native-gesture-handler";
 import { refApiService } from "../../services";
 import { take } from "rxjs/operators";
 import { SystemActions } from "../../store/actions/SystemAction";
@@ -19,12 +18,9 @@ interface IAuthSelfProps {
     // store props
     _onChangeScreen: () => void;
     _onChangeSerialNumber: (serialNumber: string) => void;
-    _onChangeName: (name: string) => void;
-    _onChangeStoreId: (storeId: string) => void;
     _alertOpen: (alert: { title: string, message: string }) => void;
     _progress: number;
     _serialNumber: string;
-    _name: string;
     _currentScreen: MainNavigationScreenTypes | undefined;
 
     // self props
@@ -32,11 +28,10 @@ interface IAuthSelfProps {
 
 interface IAuthProps extends StackScreenProps<any, MainNavigationScreenTypes.LOADING>, IAuthSelfProps { }
 
-const AuthScreenContainer = React.memo(({ _progress, _serialNumber, _name, navigation, _currentScreen,
-    _alertOpen, _onChangeScreen, _onChangeSerialNumber, _onChangeName,
+const AuthScreenContainer = React.memo(({ _serialNumber, navigation, _currentScreen,
+    _alertOpen, _onChangeScreen, _onChangeSerialNumber,
 }: IAuthProps) => {
     const [serialNumber, setSerialNumber] = useState(_serialNumber);
-    const [name, setName] = useState(_name);
     const [isLicenseValid, setLicenseValid] = useState(true);
     const [showProgressBar, setShowProgressBar] = useState(false);
 
@@ -46,7 +41,6 @@ const AuthScreenContainer = React.memo(({ _progress, _serialNumber, _name, navig
 
     useEffect(() => {
         if (serialNumber) {
-            console.log(serialNumber)
             setShowProgressBar(true);
             refApiService.terminalLicenseVerify(serialNumber).pipe(
                 take(1),
@@ -75,24 +69,17 @@ const AuthScreenContainer = React.memo(({ _progress, _serialNumber, _name, navig
         }
     }, []);
 
-    const changeNameHandler = (val: string) => {
-        setName(val);
-    };
-
     const changeSerialNumHandler = (val: string) => {
         setSerialNumber(val);
     };
 
-    const authHandler = () => {
+    const authHandler = useCallback(() => {
         setShowProgressBar(true);
-        refApiService.terminalRegistry(serialNumber, name).pipe(
+        refApiService.terminalRegistration(serialNumber).pipe(
             take(1),
         ).subscribe(
             v => {
-                console.warn("OK");
                 _onChangeSerialNumber(serialNumber);
-                _onChangeName(name);
-
                 setShowProgressBar(false);
 
                 // Goto loading screen
@@ -106,26 +93,28 @@ const AuthScreenContainer = React.memo(({ _progress, _serialNumber, _name, navig
                 );
             },
             err => {
-                _alertOpen({ title: "Ошибка", message: err });
+                _alertOpen({ title: "Ошибка", message: err.message ? err.message : err });
                 setShowProgressBar(false);
             }
         );
-    };
+    }, [serialNumber]);
 
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: theme.themes[theme.name].loading.background }}>
             {
                 !isLicenseValid &&
                 <>
-                    <TextInput style={{ color: "#ffffff", backgroundColor: "#ffffff" }} placeholder="Имя терминала" onChangeText={changeNameHandler} value={name}></TextInput>
-                    <TextInput style={{ color: "#ffffff", backgroundColor: "#ffffff" }} placeholder="Серийный ключ" onChangeText={changeSerialNumHandler} value={serialNumber}></TextInput>
+                    <TextInput placeholderTextColor="#fff7009c" selectionColor="#fff700" underlineColorAndroid="#fff700"
+                    style={{ textAlign: "center", color: "#ffffff", minWidth: 140, marginBottom: 12 }} editable={!showProgressBar}
+                    placeholder="Серийный ключ" onChangeText={changeSerialNumHandler} value={serialNumber}></TextInput>
                 </>
             }
-            <SimpleButton style={{ backgroundColor: "#00ff00" }} textStyle={{ color: "#ffffff" }} onPress={authHandler} title="Аторизоваться"></SimpleButton>
+            <SimpleButton style={{ backgroundColor: "#fff700", minWidth: 140 }} textStyle={{ color: "#000000" }}
+            onPress={authHandler} title="Зарегистрировать" disabled={showProgressBar}></SimpleButton>
             {
                 !!showProgressBar &&
                 <ProgressBar
-                    style={{ width: "100%", maxWidth: 200, marginLeft: "10%", marginRight: "10%" }}
+                    style={{ width: "100%", marginTop: 12, maxWidth: 140, marginLeft: "10%", marginRight: "10%" }}
                     styleAttr="Horizontal"
                     indeterminate={true}
                     color={theme.themes[theme.name].loading.progressBar.trackColor}></ProgressBar>
@@ -138,7 +127,6 @@ const mapStateToProps = (state: IAppState, ownProps: IAuthProps) => {
     return {
         _progress: CombinedDataSelectors.selectProgress(state),
         _serialNumber: SystemSelectors.selectDeviceInfo(state)?.serialNumber,
-        _name: SystemSelectors.selectDeviceInfo(state)?.name,
         _currentScreen: CapabilitiesSelectors.selectCurrentScreen(state),
     };
 };
@@ -146,13 +134,10 @@ const mapStateToProps = (state: IAppState, ownProps: IAuthProps) => {
 const mapDispatchToProps = (dispatch: Dispatch<any>): any => {
     return {
         _onChangeScreen: () => {
-            dispatch(CapabilitiesActions.setCurrentScreen(MainNavigationScreenTypes.LOADING));
+            dispatch(CapabilitiesActions.setCurrentScreen(MainNavigationScreenTypes.AUTH));
         },
         _onChangeSerialNumber: (serialNumber: string) => {
             dispatch(SystemActions.setSerialNumber(serialNumber));
-        },
-        _onChangeName: (name: string) => {
-            dispatch(SystemActions.setName(name));
         },
         _alertOpen: (alert: { title: string, message: string }) => {
             dispatch(NotificationActions.alertOpen(alert));
