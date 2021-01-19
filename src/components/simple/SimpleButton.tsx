1d53cd271d0962844443b04387b08440bc92917c
djonnyx <djonnyx@gmail.com>
2021-01-20 00:45:01 +0300

    feat: disabled mode created

--- src/components/simple/SimpleButton.tsx
+++ src/components/simple/SimpleButton.tsx
@@ -3,14 +3,15 @@ import { View, TouchableOpacity, StyleProp, ViewStyle, TextStyle, Text } from "r
 
 interface ISimpleButtonProps {
     title: string;
+    disabled?: boolean;
     style?: StyleProp<ViewStyle>;
     textStyle?: StyleProp<TextStyle>;
     onPress: () => void;
 }
 
-export const SimpleButton = ({ title, style, textStyle, onPress }: ISimpleButtonProps) => {
+export const SimpleButton = ({ title, style, textStyle, disabled = false, onPress }: ISimpleButtonProps) => {
     return (
-        <TouchableOpacity style={{ borderRadius: 3, overflow: "hidden" }} onPress={onPress}>
+        <TouchableOpacity style={{ borderRadius: 3, overflow: "hidden", opacity: disabled ? 0.35 : 1 }} onPress={onPress} disabled={disabled}>
             <View
                 style={{ paddingLeft: 18, paddingRight: 18, paddingTop: 10, paddingBottom: 10, ...style as any }}
             >
