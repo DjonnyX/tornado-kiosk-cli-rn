4303c2defbc2a317469226ed5f982f2671489fb5
djonnyx <djonnyx@gmail.com>
2021-02-21 08:46:52 +0300

    hotfix: #68 1actualUpLimit value at zero fixed

--- src/core/position-wizard/PositionWizard.ts
+++ src/core/position-wizard/PositionWizard.ts
@@ -115,7 +115,7 @@ export class PositionWizard extends EventEmitter implements IPositionWizard {
     protected _actualUpLimit: number = PositionWizard.MAX_AVAILABLE_LIMIT;
     set actualUpLimit(v: number) {
         if (this._actualUpLimit !== v) {
-            this._actualUpLimit = v;
+            this._actualUpLimit = v === 0 ? PositionWizard.MAX_AVAILABLE_LIMIT : v;
             this.update();
         }
     }
