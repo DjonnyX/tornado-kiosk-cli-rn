0b861e43aec22ad10e3a3be79d15b0ca83b24ebb
djonnyx <djonnyx@gmail.com>
2021-02-24 03:22:16 +0300

    feat: #88 reset language and order type to default values implemented

--- src/core/NavigationService.tsx
+++ src/core/NavigationService.tsx
@@ -1,14 +1,19 @@
 import React, { Dispatch, useEffect } from "react";
 import { connect } from "react-redux";
+import { ICompiledLanguage, ICompiledOrderType } from "@djonnyx/tornado-types";
 import { IAppState } from "../store/state";
-import { CapabilitiesSelectors, MyOrderSelectors } from "../store/selectors";
+import { CapabilitiesSelectors, CombinedDataSelectors, MyOrderSelectors } from "../store/selectors";
 import { MainNavigationScreenTypes } from "../components/navigation";
-import { NotificationActions } from "../store/actions";
+import { CapabilitiesActions, CombinedDataActions, NotificationActions } from "../store/actions";
 
 interface INavigationServiceProps {
     // store
+    _setLanguage?: (language: ICompiledLanguage) => void;
+    _setOrderType?: (orderType: ICompiledOrderType) => void;
     _snackClose?: () => void;
     _alertClose?: () => void;
+    _languages?: Array<ICompiledLanguage>;
+    _orderTypes?: Array<ICompiledOrderType>;
     _orderStateId?: number;
     _currentScreen?: MainNavigationScreenTypes;
 
@@ -16,15 +21,27 @@ interface INavigationServiceProps {
     onNavigate?: (screen: MainNavigationScreenTypes) => void;
 }
 
-export const NavigationServiceContainer = React.memo(({ onNavigate, _orderStateId, _currentScreen,
-    _alertClose, _snackClose }: INavigationServiceProps) => {
+export const NavigationServiceContainer = React.memo(({ onNavigate, _orderStateId, _currentScreen, _languages, _orderTypes,
+    _setLanguage, _setOrderType, _alertClose, _snackClose }: INavigationServiceProps) => {
 
     useEffect(() => {
         if (_orderStateId === 0 && (_currentScreen === MainNavigationScreenTypes.MENU
             || _currentScreen === MainNavigationScreenTypes.CONFIRMATION_ORDER)) {
-            
+
             if (_alertClose !== undefined) _alertClose();
             if (_snackClose !== undefined) _snackClose();
+            if (_setLanguage !== undefined) {
+                const defaultLanguage = _languages?.find(lang => lang.isDefault);
+                if (!!defaultLanguage) {
+                    _setLanguage(defaultLanguage);
+                }
+            }
+            if (_setOrderType !== undefined) {
+                const defaultOrderType = !!_orderTypes && _orderTypes?.length > 0 ? _orderTypes[0] : undefined;
+                if (!!defaultOrderType) {
+                    _setOrderType(defaultOrderType);
+                }
+            }
             if (onNavigate !== undefined) onNavigate(MainNavigationScreenTypes.INTRO);
         }
         else
@@ -39,6 +56,8 @@ export const NavigationServiceContainer = React.memo(({ onNavigate, _orderStateI
 
 const mapStateToProps = (state: IAppState) => {
     return {
+        _orderTypes: CombinedDataSelectors.selectOrderTypes(state),
+        _languages: CombinedDataSelectors.selectLangages(state),
         _orderStateId: MyOrderSelectors.selectStateId(state),
         _currentScreen: CapabilitiesSelectors.selectCurrentScreen(state),
     };
@@ -46,6 +65,12 @@ const mapStateToProps = (state: IAppState) => {
 
 const mapDispatchToProps = (dispatch: Dispatch<any>) => {
     return {
+        _setLanguage: (language: ICompiledLanguage) => {
+            dispatch(CapabilitiesActions.setLanguage(language));
+        },
+        _setOrderType: (orderType: ICompiledOrderType) => {
+            dispatch(CapabilitiesActions.setOrderType(orderType));
+        },
         _snackClose: () => {
             dispatch(NotificationActions.snackClose());
         },
