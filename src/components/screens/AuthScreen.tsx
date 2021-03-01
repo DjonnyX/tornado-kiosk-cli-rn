eec850f2431349ca7064654cfa511be039e3ac62
djonnyx <djonnyx@gmail.com>
2021-03-01 18:32:04 +0300

    feat: #47 #48 auth forms validation implemented

--- src/components/screens/AuthScreen.tsx
+++ src/components/screens/AuthScreen.tsx
@@ -2,7 +2,7 @@ import React, { Dispatch, useCallback, useEffect, useState } from "react";
 import { StackScreenProps } from "@react-navigation/stack";
 import { ProgressBar } from "@react-native-community/progress-bar-android";
 import { Picker } from '@react-native-community/picker';
-import { View, TextInput } from "react-native";
+import { View, TextInput, Text } from "react-native";
 import { MainNavigationScreenTypes } from "../navigation";
 import { IAppState } from "../../store/state";
 import { connect } from "react-redux";
@@ -113,7 +113,7 @@ const AuthScreenContainer = React.memo(({ _serialNumber, _setupStep, _terminalId
                         title: "Ошибка", message: err.message ? err.message : err, buttons: [
                             {
                                 title: "Отмена",
-                                action: () => {}
+                                action: () => { }
                             },
                             {
                                 title: "Повторить",
@@ -199,6 +199,11 @@ const AuthScreenContainer = React.memo(({ _serialNumber, _setupStep, _terminalId
         );
     }, [terminalName, storeId, _terminalId]);
 
+    const isStep1Valid = serialNumber !== undefined && serialNumber.length > 0;
+    const isTerminalNameValid = terminalName !== undefined && terminalName.length > 0;
+    const isStoreIdValid = storeId !== undefined && storeId.length > 1;
+    const isStep2Valid = isTerminalNameValid && isStoreIdValid;
+
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: theme.themes[theme.name].loading.background }}>
             {
@@ -208,32 +213,88 @@ const AuthScreenContainer = React.memo(({ _serialNumber, _setupStep, _terminalId
                         // Enter serial number
                         _setupStep === 0 &&
                         <>
-                            <TextInput placeholderTextColor="#fff7009c" selectionColor="#fff700" underlineColorAndroid="#fff700"
-                                style={{ textAlign: "center", color: "#ffffff", minWidth: 140, marginBottom: 12 }} editable={!showProgressBar}
-                                placeholder="Серийный ключ" onChangeText={changeSerialNumHandler} value={serialNumber}></TextInput>
-                            <SimpleButton style={{ backgroundColor: "#fff700", minWidth: 140 }} textStyle={{ color: "#000000" }}
-                                onPress={authHandler} title="Зарегистрировать" disabled={showProgressBar}></SimpleButton>
+                            <View style={{ marginBottom: 12 }}>
+                                <TextInput keyboardType="number-pad" placeholderTextColor={theme.themes[theme.name].service.textInput.placeholderColor}
+                                    selectionColor={theme.themes[theme.name].service.textInput.selectionColor}
+                                    underlineColorAndroid={isStep1Valid
+                                        ? theme.themes[theme.name].service.textInput.underlineColor
+                                        : theme.themes[theme.name].service.textInput.underlineWrongColor
+                                    }
+                                    style={{
+                                        fontSize: 16,
+                                        textAlign: "center", color: theme.themes[theme.name].service.textInput.textColor,
+                                        minWidth: 140, marginBottom: 12
+                                    }} editable={!showProgressBar}
+                                    placeholder="Серийный ключ" onChangeText={changeSerialNumHandler} value={serialNumber} />
+                                {
+                                    !isStep1Valid &&
+                                    <Text style={{ fontSize: 12, color: theme.themes[theme.name].service.errorLabel.textColor }}>
+                                        * Обязательное поле
+                                </Text>
+                                }
+                            </View>
+                            <SimpleButton style={{ backgroundColor: theme.themes[theme.name].service.button.backgroundColor, minWidth: 180 }}
+                                textStyle={{ fontSize: 16, color: theme.themes[theme.name].service.button.textColor }}
+                                onPress={authHandler} title="Зарегистрировать" disabled={showProgressBar || !isStep1Valid} />
                         </>
                     }
                     {
                         // Enter terminal name and store
                         _setupStep === 1 &&
                         <>
-                            <TextInput placeholderTextColor="#fff7009c" selectionColor="#fff700" underlineColorAndroid="#fff700"
-                                style={{ textAlign: "center", color: "#ffffff", minWidth: 140, marginBottom: 12 }} editable={!showProgressBar}
-                                placeholder="Имя терминала" onChangeText={changeTerminalNameHandler} value={terminalName}></TextInput>
-                            <Picker
-                                selectedValue={storeId}
-                                style={{ textAlign: "center", minWidth: 140, color: "#fff7009c", borderWidth: 2, borderColor: "#fff7009c", borderRadius: 4 }}
-                                onValueChange={(itemValue, itemIndex) => setStoreId(String(itemValue))}
-                            >
+                            <View style={{ marginBottom: 12 }}>
+                                <TextInput keyboardType="default" placeholderTextColor={theme.themes[theme.name].service.textInput.placeholderColor}
+                                    selectionColor={theme.themes[theme.name].service.textInput.selectionColor}
+                                    underlineColorAndroid={isTerminalNameValid
+                                        ? theme.themes[theme.name].service.textInput.underlineColor
+                                        : theme.themes[theme.name].service.textInput.underlineWrongColor
+                                    }
+                                    style={{
+                                        fontSize: 16,
+                                        textAlign: "center", color: theme.themes[theme.name].service.textInput.textColor,
+                                        minWidth: 180
+                                    }} editable={!showProgressBar}
+                                    placeholder="Название терминала" onChangeText={changeTerminalNameHandler} value={terminalName} />
                                 {
-                                    stores.map(store => <Picker.Item label={store.name} value={store.id || ""} />)
-
+                                    !isTerminalNameValid &&
+                                    <Text style={{ fontSize: 12, color: theme.themes[theme.name].service.errorLabel.textColor }}>
+                                        * Обязательное поле
+                                </Text>
                                 }
-                            </Picker>
-                            <SimpleButton style={{ backgroundColor: "#fff700", minWidth: 140 }} textStyle={{ color: "#000000" }}
-                                onPress={saveParamsHandler} title="Сохранить" disabled={showProgressBar}></SimpleButton>
+                            </View>
+                            <View style={{ marginBottom: 12 }}>
+                                <Picker
+                                    mode="dropdown"
+                                    selectedValue={storeId}
+                                    style={{
+                                        textAlign: "center", minWidth: 180,
+                                        color: isStoreIdValid
+                                            ? theme.themes[theme.name].service.picker.textColor
+                                            : theme.themes[theme.name].service.textInput.placeholderColor,
+                                    }}
+                                    onValueChange={(itemValue, itemIndex) => {
+                                        if (itemIndex > 0) {
+                                            setStoreId(String(itemValue));
+                                        }
+                                    }}
+                                >
+                                    <Picker.Item color={theme.themes[theme.name].service.textInput.placeholderColor} value=""
+                                        label='Выберите магазин' />
+                                    {
+                                        stores.map(store => <Picker.Item color="black" label={store.name} value={store.id || ""} />)
+
+                                    }
+                                </Picker>
+                                {
+                                    !isStoreIdValid &&
+                                    <Text style={{ fontSize: 12, color: theme.themes[theme.name].service.errorLabel.textColor }}>
+                                        * Обязательное поле
+                                    </Text>
+                                }
+                            </View>
+                            <SimpleButton style={{ backgroundColor: theme.themes[theme.name].service.button.backgroundColor, minWidth: 180 }}
+                                textStyle={{ fontSize: 16, color: theme.themes[theme.name].service.button.textColor }}
+                                onPress={saveParamsHandler} title="Сохранить" disabled={showProgressBar || !isStep2Valid} />
                         </>
                     }
                 </>
