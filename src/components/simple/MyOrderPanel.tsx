136af8c95407d973d86848a607d64fe0f204656a
djonnyx <djonnyx@gmail.com>
2021-03-29 02:43:29 +0300

    feat: #27 ux improved

--- src/components/simple/MyOrderPanel.tsx
+++ src/components/simple/MyOrderPanel.tsx
@@ -30,12 +30,12 @@ export const MyOrderPanel = React.memo(({ orderStateId, currency, language, lang
             style={{ flex: 1, backgroundColor: theme.themes[theme.name].menu.orderPanel.backgroundColor }}
         >
             <View style={{ padding: 16, alignItems: "center" }}>
-                <View style={{ margin: "auto", marginTop: 12, marginBottom: 12, alignItems: "center" }}>
+                <View style={{ margin: "auto", marginTop: 12, marginBottom: 20, alignItems: "center" }}>
                     <LanguagePicker language={language} languages={languages} onSelect={onChangeLanguage}></LanguagePicker>
                 </View>
                 {
                     !!orderTypes && orderTypes.length > 0 &&
-                    <View style={{ margin: "auto", marginBottom: 12, alignItems: "center" }}>
+                    <View style={{ margin: "auto", marginBottom: 20, alignItems: "center" }}>
                         <OrderTypesPicker isShow={isShowOrderTypes} language={language} orderType={orderType}
                             orderTypes={orderTypes} onSelect={onChangeOrderType}
                             style={{
@@ -45,28 +45,35 @@ export const MyOrderPanel = React.memo(({ orderStateId, currency, language, lang
                             textStyle={{ color: theme.themes[theme.name].orderTypePicker.textColor }} />
                     </View>
                 }
-                <View style={{ margin: "auto", marginBottom: 20, alignItems: "center" }}>
-                    <Text style={{
+                {
+                    OrderWizard.current.positions.length !== 0 &&
+                    <View style={{ margin: "auto", marginBottom: 12, alignItems: "center" }}>
+                        {/*<Text style={{
                         textTransform: "uppercase", fontWeight: "bold", fontSize: 18,
                         color: theme.themes[theme.name].menu.sum.description.textColor
                     }}>
                         Итого
                     </Text>
-                    <Text style={{ fontWeight: "bold", fontSize: 18, color: theme.themes[theme.name].menu.sum.price.textColor }}>
-                        {
-                            OrderWizard.current.getFormatedSum(true)
-                        }
-                    </Text>
-                </View>
+                    */}
+                        <Text style={{ fontWeight: "bold", fontSize: 18, color: theme.themes[theme.name].menu.sum.price.textColor }}>
+                            {
+                                OrderWizard.current.getFormatedSum(true)
+                            }
+                        </Text>
+                    </View>
+                }
             </View>
             <View style={{ flex: 1, flexGrow: 1, margin: "auto" }}>
                 <MyOrderList />
             </View>
             <View style={{ flex: 0, height: 144, margin: "auto", padding: 24 }}>
-                <CtrlMenuButton text="Заказать" disabled={OrderWizard.current.positions.length === 0}
-                    gradient={theme.themes[theme.name].menu.ctrls.confirmButton.backgroundColor}
-                    gradientDisabled={theme.themes[theme.name].menu.ctrls.confirmButton.disabledBackgroundColor}
-                    onPress={onConfirm}></CtrlMenuButton>
+                {
+                    OrderWizard.current.positions.length !== 0 &&
+                    <CtrlMenuButton text="Заказать" disabled={OrderWizard.current.positions.length === 0}
+                        gradient={theme.themes[theme.name].menu.ctrls.confirmButton.backgroundColor}
+                        gradientDisabled={theme.themes[theme.name].menu.ctrls.confirmButton.disabledBackgroundColor}
+                        onPress={onConfirm}></CtrlMenuButton>
+                }
             </View>
         </View>
     )
